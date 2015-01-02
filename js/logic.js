012ef5d17bd7db95dca5f9a9af8a9dd371835f19
Caglar Yanik <caglar.yanik@proemion.com>
2015-01-02 17:25:23 +0100

    customized original BBE

--- js/logic.js
+++ js/logic.js
@@ -1,5 +1,27 @@
-jQuery(document).ready(function($) {
+// Copyright Caglar Yanik, 2014
 
-	// YOUR SCRIPT HERE
+jQuery(document).ready(function($) {
+		
+		// bootstrap dropdown and toggable menu
+		// $('.navbar .parent > a').addClass('dropdown-toggle');
+		// $('.navbar .dropdown-toggle').attr('data-toggle','dropdown');
+		// $('.navbar .dropdown-toggle').append(' <b class="caret"></b>');
+		
+		// alle Bilder responive machen
+		$('main img').addClass('img-responsive');
 	
+		//Check to see if the window is top if not then display button
+		$(window).scroll(function(){
+			if ($(this).scrollTop() > 100) {
+				$('.scrollToTop').fadeIn();
+			} else {
+				$('.scrollToTop').fadeOut();
+			}
+		});
+	
+		//Click event to scroll to top
+		$('.scrollToTop').click(function(){
+			$('html, body').animate({scrollTop : 0},800);
+			return false;
+		});
 });
